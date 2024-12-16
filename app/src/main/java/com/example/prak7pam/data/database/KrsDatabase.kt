026668d323f172057ce68a624a5dc4a2d10b0913
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-16 23:30:12 +0700

    Pembuatan isi Companion Object

--- app/src/main/java/com/example/prak7pam/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/prak7pam/data/database/KrsDatabase.kt
@@ -14,6 +14,18 @@ abstract class KrsDatabase : RoomDatabase(){
     abstract fun mahasiswaDao(): MahasiswaDao
 
     companion object {
+        @Volatile
+        private var Instance: KrsDatabase? = null
 
+        fun getDatabase(context: Context): KrsDatabase {
+            return (Instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context,
+                    KrsDatabase::class.java,
+                    "KrsDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
     }
 }
