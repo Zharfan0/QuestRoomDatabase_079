2bfd5cea985b6be6f38f6764ad2e2452a1c78792
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-16 23:26:09 +0700

    Atribut Mahasiswa

--- app/src/main/java/com/example/prak7pam/data/entity/Mahasiswa.kt
+++ app/src/main/java/com/example/prak7pam/data/entity/Mahasiswa.kt
@@ -6,4 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "mahasiswa")
 class Mahasiswa (
+    @PrimaryKey
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+    val jenisKelamin: String,
+    val kelas: String,
+    val angkatan: String,
     )
