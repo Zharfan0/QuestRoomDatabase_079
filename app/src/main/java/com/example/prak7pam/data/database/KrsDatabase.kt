34022c4334ae4c4a61f7c03518b15f853d3c56bc
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-16 23:28:53 +0700

    Pembuatan Companion Object

--- app/src/main/java/com/example/prak7pam/data/database/KrsDatabase.kt
+++ app/src/main/java/com/example/prak7pam/data/database/KrsDatabase.kt
@@ -1,1 +1,19 @@
 package com.example.prak7pam.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.prak7pam.data.dao.MahasiswaDao
+import com.example.prak7pam.data.entity.Mahasiswa
+
+
+@Database(entities = [Mahasiswa::class], version = 1, exportSchema = false)
+abstract class KrsDatabase : RoomDatabase(){
+
+    abstract fun mahasiswaDao(): MahasiswaDao
+
+    companion object {
+
+    }
+}
