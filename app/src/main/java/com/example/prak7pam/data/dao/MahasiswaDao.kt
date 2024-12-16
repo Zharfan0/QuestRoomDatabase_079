211ba9a0ddbf85342a1d72559d5acdd8b1b06302
ZHARFAN <zharfanwidyadana@gmail.com>
2024-12-16 23:31:09 +0700

    Pembuatan Fungsi get all data pada MahasiswaDao.kt

--- app/src/main/java/com/example/prak7pam/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/prak7pam/data/dao/MahasiswaDao.kt
@@ -1,1 +1,17 @@
 package com.example.prak7pam.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.prak7pam.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface MahasiswaDao {
+    //fungsi get all data
+    @Query("select * from mahasiswa")
+    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
+}
